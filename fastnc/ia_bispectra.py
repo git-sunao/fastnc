55db9c2fd48fa748f39ef3609ad64a664fde5ebd
Rafael Gomes <rafaelchgomes@gmail.com>
2026-04-01 14:12:15 -0400

    directs funcs tatt

--- fastnc/ia_bispectra.py
+++ fastnc/ia_bispectra.py
@@ -393,39 +393,207 @@ class BispectraIA:
         term3 = delta_K_0m3 * delta_K_0m1 * F_31_m2
         return term1 + term2 + term3
 
-    # def get_cg(self, z):
-    #     # Get params
-    #     z_piv     = self.ia_params['z_piv']
-    #     A1        = self.ia_params['A1']
-    #     alphaIA   = self.ia_params['alphaIA']
-    #     A2        = self.ia_params['A2']
-    #     alphaIA_2 = self.ia_params['alphaIA_2']
-    #     bias_ta   = self.ia_params['bias_ta']
+        # ------------------------------------------------------------------
+        # BδδE  —  Eq. 51
+        # ------------------------------------------------------------------
 
-    #     #Get C1, C1delta and C2 params
-    #     c1rhocrit = 0.0134
-    #     C1 = -A1 * ((1+z)/(1+z_piv))**alphaIA * c1rhocrit * self.cosmo['Om0'] / self.z2lgr(z)
-    #     C1delta = bias_ta*C1
-    #     C2 = A2 * ((1+z)/(1+z_piv))**alphaIA_2 * 5 * c1rhocrit * self.cosmo['Om0'] / self.z2lgr(z)**2
+    def get_B_ddE_gomes(self, k1_mag, k2_mag, k3_mag,
+                        PL1, PL2, PL3, C1, C1delta, C2, Ct=0):
+        """
+        Eq. 51 of Gomes et al. (2026).
+        BδδE(k1,k2,k3) = C1 * B^Tree_ddd
+            + C2 * PL1 PL2 [ 1/2 (k1h.k2h)(k1hx k2hx - k1hy k2hy)
+                             - 1/12 (k1hx^2 - k1hy^2 + k2hx^2 - k2hy^2) ]
+            + C1delta * PL1 PL2 [ 1/2 (k1hx^2 - k1hy^2 + k2hx^2 - k2hy^2) ]
 
-    #     # normalization to match the A1 of TATT, according to Eq. 66 of Bakx et al (1) (2025) and Eq. 30 of Bakx et. al. (2) (2025)
-    #     C1 *= 2
+        With optional velocity-shear addition from Eq. 62.
+        """
+        B_tree = self._B_ddd(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3)
 
-    #     #Get cg1, cg2_2 and cg2_3 params
-    #     cg1 = C1
-    #     cg2_2 = C2
-    #     cg2_3 = 0.5 * (3 * C1delta - C2)
+        k1hx, k1hy, k2hx, k2hy = self._khat_xy(k1_mag, k2_mag, k3_mag)
+        k1dk2 = k1hx * k2hx + k1hy * k2hy
+        diff1 = k1hx ** 2 - k1hy ** 2
+        diff2 = k2hx ** 2 - k2hy ** 2
 
-    #     return cg1, cg2_2, cg2_3
+        result = (
+                C1 * B_tree
+                + C2 * PL1 * PL2 * (
+                        0.5 * k1dk2 * (k1hx * k2hx - k1hy * k2hy)
+                        - (1.0 / 12.0) * (diff1 + diff2)
+                )
+                + C1delta * PL1 * PL2 * (
+                        0.5 * (diff1 + diff2)
+                )
+        )
 
-    def get_B_dEE(self, k1_mag, k2_mag, k3_mag, z, PL1=None, PL2=None, PL3=None, cg1=None, cg2_2=None, cg2_3=None):
+        # Velocity-shear extension (Eq. 62)
+        if Ct != 0:
+            result += -Ct * PL1 * PL2 * (
+                    (2.0 / 7.0) * (1.0 - k1dk2 ** 2)
+                    + 0.1 * (diff1 + diff2)
+                    + 0.2 * k1dk2 * (k1hx * k2hx - k1hy * k2hy)
+            )
 
-        # B_delta_delta_E (Eq. 12)
-        B_002_0 = self.get_B002(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 0)
-        B_002_2 = self.get_B002(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, cg1, cg2_2, cg2_3, 2)
-        B_ddE = 0.5 * (np.sqrt(3 / 2) * B_002_0 - B_002_2)
+        return result
 
-        # Get terms of TATT IA bispectrum
+'''Alternative functions for debugging that use the direct eqs from Gomes et. al.'''
+        # ------------------------------------------------------------------
+        # BδEE  —  Eq. 52
+        # ------------------------------------------------------------------
+
+    def get_B_dEE_gomes(self, k1_mag, k2_mag, k3_mag,
+                        PL1, PL2, PL3, C1, C1delta, C2, Ct=0):
+        """
+        Eq. 52 of Gomes et al. (2026).
+        BδEE(k1,k2,k3) =
+            C1^2 * B^Tree_ddd
+            + (1/3) C1 C2 * U * PL1 PL2
+            + C1 C1delta * U * PL1 PL2
+            + C1 C2 * [V + (2/3) W] * PL1 PL3
+            - C1 C1delta * W * PL1 PL3
+
+        With optional velocity-shear addition from Eq. 63.
+        """
+        B_tree = self._B_ddd(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3)
+        k1dk2, k1dk3, k2dk3 = self._dot_products(k1_mag, k2_mag, k3_mag)
+
+        U = self._U_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+        V = self._V_aux(k1_mag, k2_mag, k3_mag, k1dk2, k1dk3)
+        W = self._W_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+
+        result = (
+                C1 ** 2 * B_tree
+                + (1.0 / 3.0) * C1 * C2 * U * PL1 * PL2
+                + C1 * C1delta * U * PL1 * PL2
+                + C1 * C2 * (V + (2.0 / 3.0) * W) * PL1 * PL3
+                - C1 * C1delta * W * PL1 * PL3
+        )
+
+        # Velocity-shear extension (Eq. 63)
+        if Ct != 0:
+            U2 = self._U2_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+            V2 = self._V2_aux(k1_mag, k2_mag, k3_mag, k1dk2, k1dk3)
+            result += C1 * Ct * U2 * PL1 * PL2
+            result += C1 * Ct * V2 * PL1 * PL3
+
+        return result
+
+        # ------------------------------------------------------------------
+        # BEEE  —  Eq. 56
+        # ------------------------------------------------------------------
+
+    def get_B_EEE_gomes(self, k1_mag, k2_mag, k3_mag,
+                        PL1, PL2, PL3, C1, C1delta, C2, Ct=0):
+        """
+        Eq. 56 of Gomes et al. (2026).
+        BEEE(k1,k2,k3) =
+            C1^3 * B^Tree_ddd
+            + (1/6) C1^2 C2 * U(k1,k2,k3) * PL1 PL2
+            + (1/2) C1^2 C1delta * U(k1,k2,k3) * PL1 PL2
+            + C1^2 C2 * [T(k1,k2,k3) + (1/3) W(k1,k2,k3)] * PL1 PL3
+            - (1/2) C1^2 C1delta * W(k1,k2,k3) * PL1 PL3
+            + C1^2 C2 * [T(k2,k1,k3) + (1/3) W(k1,k2,k3)] * PL2 PL3
+            - (1/2) C1^2 C1delta * W(k2,k1,k3) * PL2 PL3
+
+        With optional velocity-shear addition from Eq. 66.
+        """
+        B_tree = self._B_ddd(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3)
+        k1dk2, k1dk3, k2dk3 = self._dot_products(k1_mag, k2_mag, k3_mag)
+
+        U_12 = self._U_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+
+        # W(k1,k2,k3) uses k1_mag, k2_mag, k3_mag, k1dk2
+        W_12 = self._W_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+
+        # W(k2,k1,k3) — swap k1<->k2 in W definition
+        # W(ka,kb,kc) = -1/(2 kc^2) * [ (ka_hat . kb_hat)^2 (ka^2 + kc^2) - ka^2 ]
+        # So W(k2,k1,k3) uses k2_mag as "ka", k1_mag as "kb", k3_mag as "kc",
+        # and the dot product k2hat.k1hat = k1dk2
+        W_21 = self._W_aux(k2_mag, k1_mag, k3_mag, k1dk2)
+
+        # T(k1,k2,k3) uses k1dk2 and k1dk3
+        T_12 = self._T_aux(k1_mag, k2_mag, k3_mag, k1dk2, k1dk3)
+
+        # T(k2,k1,k3) — swap k1<->k2 in T definition
+        # T(ka,kb,kc) uses ka_hat.kb_hat and ka_hat.kc_hat
+        # So T(k2,k1,k3) needs k2hat.k1hat = k1dk2 and k2hat.k3hat = k2dk3
+        T_21 = self._T_aux(k2_mag, k1_mag, k3_mag, k1dk2, k2dk3)
+
+        C1sq = C1 ** 2
+
+        result = (
+                C1 ** 3 * B_tree
+                + (1.0 / 6.0) * C1sq * C2 * U_12 * PL1 * PL2
+                + 0.5 * C1sq * C1delta * U_12 * PL1 * PL2
+                + C1sq * C2 * (T_12 + (1.0 / 3.0) * W_12) * PL1 * PL3
+                - 0.5 * C1sq * C1delta * W_12 * PL1 * PL3
+                + C1sq * C2 * (T_21 + (1.0 / 3.0) * W_12) * PL2 * PL3
+                - 0.5 * C1sq * C1delta * W_21 * PL2 * PL3
+        )
+
+        # Velocity-shear extension (Eq. 66)
+        if Ct != 0:
+            U2_12 = self._U2_aux(k1_mag, k2_mag, k3_mag, k1dk2)
+            V2_13 = self._V2_aux(k1_mag, k2_mag, k3_mag, k1dk2, k1dk3)
+            V2_23 = self._V2_aux(k2_mag, k1_mag, k3_mag, k1dk2, k2dk3)
+            result += 0.5 * C1sq * Ct * U2_12 * PL1 * PL2
+            result += 0.5 * C1sq * Ct * V2_13 * PL1 * PL3
+            result += 0.5 * C1sq * Ct * V2_23 * PL2 * PL3
+
+        return result
+
+        # ------------------------------------------------------------------
+        # Velocity-shear auxiliary functions (Eqs. 64-65)
+        # ------------------------------------------------------------------
+
+    def _U2_aux(self, k1_mag, k2_mag, k3_mag, k1dk2):
+        """
+        Eq. 64: U2(k1, k2, k3)
+        """
+        k1sq = k1_mag ** 2
+        k2sq = k2_mag ** 2
+        k3sq = k3_mag ** 2
+        k1k2 = k1_mag * k2_mag
+        k1dk2_sq = k1dk2 ** 2
+
+        numer = (
+                7.0 * k1dk2 * (
+                k1_mag ** 6 + k2_mag ** 6
+                - k3sq * (k1_mag ** 4 + k2_mag ** 4)
+                + 9.0 * (k1sq * k2_mag ** 4 + k1_mag ** 4 * k2sq)
+                + 2.0 * k1sq * k2sq * k3sq
+        )
+                + (10.0 + 32.0 * k1dk2_sq) * (
+                        k1_mag ** 5 * k2_mag
+                        + k2_mag ** 5 * k1_mag
+                        + 2.0 * k1_mag ** 3 * k2_mag ** 3
+                )
+                + 2.0 * k1dk2_sq * (
+                        20.0 * k1_mag ** 3 * k2_mag ** 3
+                        - 14.0 * k1k2 * k3sq * (k1sq + k2sq)
+                )
+                + 2.0 * k1sq * k2sq * k1dk2 ** 3 * (
+                        22.0 * k1sq + 22.0 * k2sq - k3sq
+                )
+                + 16.0 * k1dk2 ** 4 * k1_mag ** 3 * k2_mag ** 3
+        )
+        denom = 70.0 * k1k2 * k3_mag ** 4
+        return -numer / denom
+
+    def _V2_aux(self, k1_mag, k2_mag, k3_mag, k1dk2, k1dk3):
+        """
+        Eq. 65: V2(k1, k2, k3)
+        V2 = (2/5) * (k1/k3) * (k1hat.k3hat) * ((k1hat.k2hat)^2 - 1)
+             - (1/35) * (5 + 2*(k1hat.k3hat)^2)
+        """
+        return (
+                (2.0 / 5.0) * (k1_mag / k3_mag) * k1dk3 * (k1dk2 ** 2 - 1.0)
+                - (1.0 / 35.0) * (5.0 + 2.0 * k1dk3 ** 2)
+        )
+
+    '''Here the direct expressions end'''
+
+    '''Main function to get the IA bispectra'''
 
     def get_ia_bispectra(self, k1_mag, k2_mag, k3_mag, z, z_piv, A1, alphaIA, A2, alphaIA_2, bias_ta, Ct=0, remove_alignment=False,
                          do_non_linear=True):
@@ -436,7 +604,8 @@ class BispectraIA:
         C1delta = bias_ta * C1
         C2 = A2 * ((1 + z) / (1 + z_piv)) ** alphaIA_2 * 5 * c1rhocrit * self.cosmo['Om0'] / self.z2lgr(z) ** 2
 
-        #Ct = -C2/5 #HARD CODED Ct FOR PAPER RUN
+        Ct = C1*Ct #only for paper plots, after this we will have to adjust it better
+
         # Get cg1, cg2_2 and cg2_3 params
         cg1 = 2*C1 # normalization to match the A1 of TATT, according to Eq. 66 of Bakx et al (1) (2025) and Eq. 30 of Bakx et. al. (2) (2025)
         cg2_1 = -2*Ct/5
@@ -557,6 +726,20 @@ class BispectraIA:
 
         # B_BBB
         B_BBB = 0
+
+        direct_methods = False
+
+        if direct_methods:
+            B_ddE = self.get_B_ddE_gomes(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, C1, C1delta, C2)
+
+            #try change
+            B_dEd = self.get_B_ddE_gomes(k2_mag, k3_mag, k1_mag, PL2, PL3, PL1, C1, C1delta, C2)
+            B_Edd = self.get_B_ddE_gomes(k3_mag, k1_mag, k2_mag, PL3, PL1, PL2, C1, C1delta, C2)
+            B_dEE = self.get_B_dEE_gomes(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, C1, C1delta, C2)
+            B_EdE = self.get_B_dEE_gomes(k2_mag, k3_mag, k1_mag, PL2, PL3, PL1, C1, C1delta, C2)
+            B_EEd = self.get_B_dEE_gomes(k3_mag, k1_mag, k2_mag, PL3, PL1, PL2, C1, C1delta, C2)
+            B_EEE = self.get_B_EEE_gomes(k1_mag, k2_mag, k3_mag, PL1, PL2, PL3, C1, C1delta, C2)
+            print("did direct method gomes")
 
         tl_mat = 2 * (self.F2_tree(k1_mag, k2_mag, k3_mag) * PL1 * PL2 + self.F2_tree(k2_mag, k3_mag, k1_mag) * PL2 * PL3 +
                                  self.F2_tree(k3_mag, k1_mag, k2_mag) * PL3 * PL1)
